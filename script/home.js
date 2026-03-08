113cf61d206865e44514750c4cc1f4caab7003e0
sabbirhossain778 <mshb5778@gmail.com>
2026-03-08 07:46:42 +0600

    succesfully complete one home card

--- script/home.js
+++ script/home.js
@@ -1,1 +1,20 @@
+const loadAllIssues = () => {
+    fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+    .then((res) => res.json())
+    .then((json) => displayIssues(json.data))
+}
 
+const loadAllCard = () => {
+    console.log();
+    
+}
+
+const displayIssues = (issues) =>{
+    // console.log(issues);
+    for(let i = 0; i < issues.length; i++){
+        const issue = issues[i];
+        console.log(issue.title);
+    }
+    
+}
+loadAllIssues();
